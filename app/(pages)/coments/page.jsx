83af3d5b0cb996fc2aa3aa934340dba3332daf0a
atefehmehrabi76 <mehrabiatefeh12@gmail.com>
2025-02-24 15:47:51 +0330

    update coment page

--- app/(pages)/coments/page.jsx
+++ app/(pages)/coments/page.jsx
@@ -3,7 +3,11 @@ import React from "react";
 function Coment() {
   return (
     <form className="mx-[50px] my-[auto] p-5 w-[600px] h-[400px] flex flex-col gap-10 bg-white ">
-      <input type="text" placeholder="نام کاربری خود را وارد کنید" className="w-[250px]" />
+      <input
+        type="text"
+        placeholder="نام کاربری خود را وارد کنید"
+        className="w-[250px]"
+      />
       <input type="text" placeholder="نظر خود را بنویسید" className="" />
       <button className="border rounded-xl bg-blue-900 text-white py-2 px-5 w-[150px] relative right-[37~0px] top-[170px]">
         ثبت نظر
