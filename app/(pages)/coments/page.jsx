f279fa28c5783f4c2fb2c57990152a0e21854a7d
atefehmehrabi76 <mehrabiatefeh12@gmail.com>
2025-02-25 14:37:41 +0330

    start localstorage

--- app/(pages)/coments/page.jsx
+++ app/(pages)/coments/page.jsx
@@ -5,33 +5,68 @@ import React, { useEffect, useState } from "react";
 function CommentPage() {
   const [comment, setComment] = useState("");
   const [user, setUser] = useState("");
-  const [outPut, setOutPut] = useState("");
-  const addComment = () => {
-    setOutPut(`user:${user} , comment :${comment}`);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      console.log("localStorage Available", localStorage);
+      const savedComments = localStorage.getItem("Comments");
+      console.log("Saved Comments:", savedComments);
+      if (savedComments) {
+       setComments( JSON.parse(savedComments))
+      }
+    }
+  }, []);
+
+  const addComment = (e) => {
+    e.preventDefault();
+    if (user.trim() === "" || comment.trim() === "") return;
+    const newComments = [...comments, { user, comment }];
+    setComments(newComments);
+    if (typeof window !== "undefined") {
+      localStorage.setItem("Comments", JSON.stringify(newComments));
+    }
+    setUser("");
+    setComment("");
   };
-  // useEffect(() => {
-  //   addComment();
-  // }, []);
+
   return (
     <>
-      <form className="mx-[50px] my-[auto] p-5 w-[600px] h-[400px] flex flex-col gap-10 bg-white ">
+      <form className="mx-[50px] my-[auto] p-5 w-[600px] h-[400px] flex flex-col gap-10 bg-white border border-slate-400 rounded-lg ">
         <input
-          onKeyUp={(e) => {
-            setUser(e.target.value);
+          onKeyUp ={(e) => {
+            setUser(e.target.value.trim());
           }}
           type="text"
+          value={user}
           placeholder="نام کاربری خود را وارد کنید"
-          className="w-[250px]"
+          className="w-[250px] shadow-lg shadow-slate-400 p-2 border border-slate-400 rounded-lg"
         />
-        <input
-          onKeyUp={(e) => setComment(e.target.value)}
+        <textarea
+          onChange={(e) => setComment(e.target.value.trim())}
           type="text"
+          value={comment}
           placeholder="نظر خود را بنویسید"
-          className=""
+          className="shadow-lg shadow-slate-400 p-2 border border-slate-400 rounded-lg"
         />
         <Button handleClick={addComment} />
       </form>
-      <div>{outPut}</div>
+
+      {comments.map((c, index) => {
+        return (
+          <div
+            key={index}
+            className="bg-white w-[50%] border border-slate-400 rounded-lg mt-20 mx-40 p-2"
+          >
+            <p className="mb-2">
+              <strong>نام کاربری :</strong> {c.user}
+            </p>
+            <p className="px-3">
+              <strong>نظر کاربر :</strong> {c.comment}
+            </p>
+          </div>
+        );
+      })}
     </>
   );
 }
--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -5,7 +5,7 @@ function Button({ handleClick }) {
     <div>
       <button
         onClick={handleClick}
-        className="border rounded-xl bg-blue-900 text-white py-2 px-5 w-[150px] relative right-[37~0px] top-[170px]"
+        className="border rounded-xl bg-blue-900 text-white py-2 px-5 w-[150px] relative right-[370px] top-[120px]"
       >
         ثبت نظر
       </button>
